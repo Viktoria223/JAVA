0b5121f62924c8941e69f70701a88fdc213d13e2
Fedorchuk_Viktoria <fedorchuk-vika03@mail.ru>
2021-03-10 21:32:44 +0300

    //

--- 1/P1.java
+++ 1/P1.java
@@ -6,24 +6,39 @@ public class P1 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         ArrayList<Integer> list = new ArrayList<>();
-        //list.add(2);
-        int y = 0;
-        int j = 1;
-        while (y != n) {
-            j++;
-            boolean f = true;
-            for (Integer i : list) {
-                if (j % i == 0) {
-                    f = false;
+        list.add(2);
+        int y = 1;
+        int j = 2;
+
+        if (n == 1) {
+            System.out.println(j);
+        } else {
+            if (n == 2) {
+                list.add(3);
+                for (Integer i : list) {
+                    System.out.print(i + " ");
+                }
+            } else {
+                list.add(3);
+                y++;
+                j = 3;
+                while (y != n) {
+                    j += 2;
+                    boolean f = true;
+                    for (Integer i : list) {
+                        if (j % i == 0) {
+                            f = false;
+                        }
+                    }
+                    if (f) {
+                        list.add(j);
+                        y++;
+                    }
+                }
+                for (Integer i : list) {
+                    System.out.print(i + " ");
                 }
             }
-            if (f) {
-                list.add(j);
-                y++;
-            }
-        }
-        for (Integer i : list) {
-            System.out.print(i + " ");
         }
     }
 }
