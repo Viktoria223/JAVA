f03336c62911306ef0491cfa8d818190c0c022fb
Fedorchuk_Viktoria <fedorchuk-vika03@mail.ru>
2021-03-13 22:32:18 +0300

    //

--- 3/P3.java
+++ 3/P3.java
@@ -6,26 +6,27 @@ public class P3 {
         ArrayList<Character> list = new ArrayList<>();
         String seq = sc.next();
         boolean isTrue = true;
-        for (int i = 0; i < seq.length(); i++) {
+        /*for (int i = 0; i < seq.length(); i++) {
             char j = seq.charAt(i);
             list.add(j);
-        }
+        }*/
         Stack<Character> stack = new Stack<>();
-        for (Character i : list) {
+        for (int z = 0; z < seq.length(); z++) {
+            char i = seq.charAt(z);
             if (i == '(' || i == '[' || i == '{') {
                 stack.push(i);
             } else {
-                /*if (stack.empty()) {
+                if (stack.empty()) {
                     isTrue = false;
                     break;
-                }*/
+                }
                 if (i == ')' && stack.peek() == '(') {
                     stack.pop();
                 }
-                if (i == ']' && stack.pop() == '[') {
+                if (i == ']' && stack.peek() == '[') {
                     stack.pop();
                 }
-                if (i == '}' && stack.pop() == '{') {
+                if (i == '}' && stack.peek() == '{') {
                     stack.pop();
                 }
             }
