5dfc149bbe1cc489d3f79bd69035ea9342fbf150
Fedorchuk_Viktoria <fedorchuk-vika03@mail.ru>
2021-03-13 23:52:13 +0300

    //

--- 3/P3.java
+++ 3/P3.java
@@ -3,19 +3,17 @@ import java.util.*;
 public class P3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String seq = sc.next();
+        String seq = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
         int uOpen = 0;
         int uClose = 0;
         boolean isTrue = true;
-        Stack<Character> stack = new Stack<>();
         for (int z = 0; z < seq.length(); z++) {
             //char i = seq.charAt(z);
             if (seq.charAt(z) == '(' || seq.charAt(z) == '[' || seq.charAt(z) == '{') {
                 stack.push(seq.charAt(z));
                 uOpen++;
-            } else {
-                uClose++;
-            }
+            } else uClose++;
             if (!stack.empty()) {
                 if (seq.charAt(z) == ')' && stack.peek() == '(') {
                     stack.pop();
