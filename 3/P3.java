10c9ddd722dd375f71ed2f2fb2d463eada0a4c13
Fedorchuk_Viktoria <fedorchuk-vika03@mail.ru>
2021-03-13 23:59:22 +0300

    //

--- 3/P3.java
+++ 3/P3.java
@@ -1,39 +1,3 @@
-/*import java.util.*;
-
-public class P3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String seq = sc.nextLine();
-        Stack<Character> stack = new Stack<>();
-        int uOpen = 0;
-        int uClose = 0;
-        boolean isTrue = true;
-        for (int z = 0; z < seq.length(); z++) {
-            //char i = seq.charAt(z);
-            if (seq.charAt(z) == '(' || seq.charAt(z) == '[' || seq.charAt(z) == '{') {
-                stack.push(seq.charAt(z));
-                uOpen++;
-            } else uClose++;
-            if (!stack.empty()) {
-                if (seq.charAt(z) == ')' && stack.peek() == '(') {
-                    stack.pop();
-                }
-                if (seq.charAt(z) == ']' && stack.peek() == '[') {
-                    stack.pop();
-                }
-                if (seq.charAt(z) == '}' && stack.peek() == '{') {
-                    stack.pop();
-                }
-            }
-        }
-        if(stack.empty() && uClose == uOpen) {
-            System.out.print("YES");
-        } else {
-            System.out.print("NO");
-        }
-    }
-}*/
-
 import java.util.*;
 
 
@@ -77,33 +41,3 @@ public class P3 {
         }
     }
 }
-
-        /*for (Character i : list) {
-            if (i == '(' || i == '[' || i == '{') {
-                stack.push(i);
-            }
-            if (i == ')' && stack.empty()) {
-                isTrue = false;
-                break;
-            }
-            if (i == ']' && stack.empty()) {
-                isTrue = false;
-                break;
-            }
-            if (i == '}' && stack.empty()) {
-                isTrue = false;
-                break;
-            }
-            if (i == ')' && stack.pop() != '(') {
-                isTrue = false;
-                break;
-            }
-            if (i == ']' && stack.pop() != '[') {
-                isTrue = false;
-                break;
-            }
-            if (i == '{' && stack.pop() != '}') {
-                isTrue = false;
-                break;
-            }
-        }*/
