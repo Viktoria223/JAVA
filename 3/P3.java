f4c59ecc7f7b567e1ffee46be7fb66ef387b5b2c
Fedorchuk_Viktoria <fedorchuk-vika03@mail.ru>
2021-03-13 22:25:22 +0300

    //

--- 3/P3.java
+++ 3/P3.java
@@ -4,54 +4,33 @@ public class P3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Character> list = new ArrayList<>();
-        ArrayList<Character> list2 = new ArrayList<>();
         String seq = sc.next();
         boolean isTrue = true;
         for (int i = 0; i < seq.length(); i++) {
             char j = seq.charAt(i);
             list.add(j);
-            list2.add(j);
         }
         Stack<Character> stack = new Stack<>();
         for (Character i : list) {
             if (i == '(' || i == '[' || i == '{') {
                 stack.push(i);
-                list2.remove(i);
             } else {
                 if (stack.empty()) {
                     isTrue = false;
                     break;
                 }
-                if (i == ')') {
-                    if (stack.pop() == '(') {
-                        list2.remove(i);
-                    } else {
-                        isTrue = false;
-                        break;
-                    }
+                if (i == ')' && stack.peek() == '(') {
+                    stack.pop();
                 }
-                if (i == ']') {
-                    if (stack.pop() == '[') {
-                        list2.remove(i);
-                    } else {
-                        isTrue = false;
-                        break;
-                    }
+                if (i == ']' && stack.pop() != '[') {
+                    stack.pop();
                 }
-                if (i == '}') {
-                    if (stack.pop() == '{') {
-                        list2.remove(i);
-                    } else {
-                        isTrue = false;
-                        break;
-                    }
+                if (i == '}' && stack.pop() != '{') {
+                    stack.pop();
                 }
             }
         }
         if (!stack.empty()) {
-            isTrue = false;
-        }
-        if (!list2.isEmpty()) {
             isTrue = false;
         }
         if (isTrue) {
@@ -91,4 +70,3 @@ public class P3 {
                 break;
             }
         }*/
-
