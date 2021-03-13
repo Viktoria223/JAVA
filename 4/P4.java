8cfdfe1c14d9f32baf48790b75fa6e5234bc4b14
Fedorchuk_Viktoria <fedorchuk-vika03@mail.ru>
2021-03-13 23:03:01 +0300

    //

---
+++ 4/P4.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class P4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Character> list = new ArrayList<>();
+        String seq = sc.next();
+        boolean isTrue = true;
+        for (int i = 0; i < seq.length(); i++) {
+            char j = seq.charAt(i);
+            list.add(j);
+        }
+        Stack<Character> stack = new Stack<>();
+        for (Character i : list) {
+            if (i == '(' || i == '[' || i == '{') {
+                stack.push(i);
+            } else {
+                if (i == ')') {
+                    if (stack.peek() == '(') {
+                        stack.pop();
+                    }
+                    if (stack.peek() != '(' || stack.empty()) {
+                        isTrue = false;
+                        break;
+                    }
+                }
+                if (i == ']') {
+                    if (stack.peek() != '[' || stack.empty()) {
+                        stack.pop();
+                    } else {
+                        isTrue = false;
+                        break;
+                    }
+                }
+                if (i == '}') {
+                    if (stack.peek() != '{' || stack.empty()) {
+                        stack.pop();
+                    } else {
+                        isTrue = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if (!stack.empty()) {
+            isTrue = false;
+        }
+        if (isTrue) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
+    }
+}
